69dbcd6055dcf697ccf77b9e2d5fed4c575499aa
Luis Rodriguez <luishi.rodriguez@gmail.com>
2023-05-09 21:56:33 -0300

    Guia 10, Ejercicio 4

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -2,8 +2,13 @@ package prueba;
 
 public class Prueba {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Esta es la primera línea");
+        Thread.sleep(1000); // Pausa de 1 segundo
+        System.out.println("Esta es la segunda línea");
+        Thread.sleep(1000); // Pausa de 1 segundo
+        System.out.println("Esta es la tercera línea");
+        Thread.sleep(1000); // Pausa de 1 segundo
+        System.out.println("Fin del programa");
     }
-    
 }
