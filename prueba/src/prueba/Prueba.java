ca96553128d229483fe844ddbcec104296f1508e
Luis Rodriguez <luishi.rodriguez@gmail.com>
2023-05-16 23:30:40 -0300

    Ejercicios extras de colecciones

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -1,9 +1,34 @@
 package prueba;
-    
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Prueba {
 
     public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("A", 1);
+        hashMap.put("B", 2);
+        hashMap.put("C", 3);
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
         
+        System.out.println("-----------------------------");
+
+        Iterator<Map.Entry<String, Integer>> iterator = hashMap.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            if (entry.getValue() == 2) {
+                iterator.remove();
+            }
+        }
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
+
     }
-    
+
 }
